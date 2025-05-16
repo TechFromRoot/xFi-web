ca101d7bf825a3eea36f79533458c7cf42d33dff
Adaobi-Chuks <chuksaginamada@gmail.com>
2025-05-16 23:00:03 +0100

    Feat: Add favicon and logo

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,14 +5,7 @@ export default function LandingPage() {
         <div className="min-h-screen flex items-center justify-center bg-black text-white py-10">
             <div className="w-full max-w-max min-w-[60vh] border-2 border-gray-700 rounded-2xl p-10 text-center flex flex-col justify-between min-h-[80vh] max-h-[90vh]">
                 <div>
-                    {/* Above the App name */}
-                    {/* <div className="flex justify-center mb-4">
-                        <img src="/logo.svg" alt="xFi Logo" className="w-12 h-12" />
-                    </div> */}
-
-                    {/* Replace App name so remove h1 tag */}
-                    {/* <img src="/logo-text.svg" alt="xFi" className="w-48 mx-auto mb-6" /> */}
-                    <h1 className="text-5xl font-bold mb-25 mt-3">xFi</h1>
+                    <img src="/xFi-logo.png" alt="xFi" className="w-48 mx-auto mb-25 mt-3" />
                     <p className="text-gray-400 mb-10 text-lg">
                         Perform DeFi actions directly on twitter.
                     </p>
